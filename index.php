667f2e14b6da74fcef46a662a6ae5512ee31ed1a
Lurk150rus <unknownsomebody@yandex.ru>
2020-12-09 19:50:59 +0300

    First
    First

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Тег SELECT</title>
+</head>
+<body>
+
+<form action="/result.php" method="get">
+    <input type="text" name="x1">
+    <select name="operation">
+        <option value="+">+</option>
+        <option value="-">-</option>
+    </select>
+    <input type="text" name="x2">
+    <input type="submit" value="Посчитать">
+</form>
+
+</body>
+</html>
